33ba700cd9df4232aaee9023edf6a312bc0d815a
Banana <banana@bananas-air.speedport_w_724v_09011603_06_010>
2025-10-20 22:50:31 +0200

    init react with router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{margin: 'auto'}}>
+      <h1>
+        Banana Games
+      </h1>
+    </div>
+  )
+}
+
+export default App
