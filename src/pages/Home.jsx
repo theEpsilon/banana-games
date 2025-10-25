ce58900d774e6320d3af16f91c683372059a4e02
theEpsilon <f.berndt99@gmail.com>
2025-10-25 18:13:49 +0200

    home page styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,27 @@
 import '../App.css'
 import { Link } from "react-router"
+import wordleImg from "../assets/wordle_titleimg.png"
+import './home.css'
+import { Container, Row } from 'react-bootstrap'
 
 function Home() {
 
   return (
-    <div style={{margin: 'auto'}}>
-      <h1>
-        Banana Games
-      </h1>
+    <Container className='home-container'>
+      <div className='mb-5 home-header'>
+        <h1>
+          Banana Games
+        </h1>
+      </div>
+      <div className='d-flex justify-content-center'>
         <Link to="/wordle">
-          <button>
-            Wordle
+          <button className='game-button'>
+            <img src={wordleImg}></img>
+            <div>Wordle</div>
           </button>
         </Link>
-    </div>
+      </div>
+    </Container>
   )
 }
 
